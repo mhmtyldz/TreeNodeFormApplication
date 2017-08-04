6c302928fc74d29b862e43736f285a88440072ca
Mahmut Yıldız <yldzmahmut0@gmail.com>
2017-08-05 01:45:03 +0300

    C sharp form yapısındaki treeview nesnesinden yararlandım.

---
+++ TreeNodeFormApplication/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreeNodeFormApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            treenodeinsert();
+        }
+
+        private void treenodeinsert()
+        {
+            treeView1.BeginUpdate();
+            treeView1.Nodes.Add("Arts & Photography");
+            treeView1.Nodes[0].Nodes.Add("Architecture");
+            treeView1.Nodes[0].Nodes.Add("Graphic Design");
+            treeView1.Nodes[0].Nodes.Add("Music");
+            treeView1.Nodes[0].Nodes[2].Nodes.Add("Songbooks");
+            treeView1.Nodes[0].Nodes[2].Nodes.Add("Instruments & Performers");
+            treeView1.Nodes[0].Nodes[2].Nodes[1].Nodes.Add("Brass");
+            treeView1.Nodes[0].Nodes[2].Nodes[1].Nodes.Add("Woodwinds");
+
+
+            treeView1.Nodes.Add("Comics & Graphic Novels");
+            treeView1.Nodes[1].Nodes.Add("Comic Strips");
+            treeView1.Nodes[1].Nodes.Add("Graphic Novels");
+            treeView1.Nodes[1].Nodes.Add("Manga");
+            
+
+            treeView1.Nodes.Add("Mystery, Thriller and Suspense");
+            treeView1.Nodes[2].Nodes.Add("Mystery");
+            treeView1.Nodes[2].Nodes[0].Nodes.Add("Hard Boiled");
+            treeView1.Nodes[2].Nodes[0].Nodes.Add("Police Procedurals");
+            treeView1.Nodes[2].Nodes[0].Nodes[1].Nodes.Add("British Detectives");
+            treeView1.Nodes[2].Nodes[0].Nodes[1].Nodes.Add("FBI Agents");
+            treeView1.Nodes[2].Nodes[0].Nodes[1].Nodes.Add("Police Officers");
+
+            treeView1.Nodes.Add("Nonfiction");
+            treeView1.Nodes[3].Nodes.Add("Biographies and Memoirs");
+            treeView1.Nodes[3].Nodes.Add("Business & Investing");
+            treeView1.Nodes[3].Nodes.Add("Computers & Technology");
+            treeView1.Nodes[3].Nodes[2].Nodes.Add("Databases");
+            treeView1.Nodes[3].Nodes[2].Nodes.Add("Hardware");
+            treeView1.Nodes[3].Nodes[2].Nodes.Add("Software");
+
+        }
+        
+
+       
+    }
+}
---
+++ TreeNodeFormApplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TreeNodeFormApplication")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TreeNodeFormApplication")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a8c93444-1042-463a-bb1f-26c5b20a6bd1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
